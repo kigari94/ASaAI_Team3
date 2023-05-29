0b23a7e544ed14450b5c8c384ad884728e57c33f
kigari94 <stefankulow@web.de>
2023-05-29 17:25:35 +0200

    adding script for HAW

--- Project/webscraping/webscraping/spiders/crawling_spider.py
+++ Project/webscraping/webscraping/spiders/crawling_spider.py
@@ -1,7 +1,19 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
+
 class CrawlingSpider(CrawlSpider):
     name = "collegeCrawler"
     allowed_domains = ["haw-hamburg.de"]
-    start_url = ["https://www.haw-hamburg.de/"]
+    start_urls = ["https://www.haw-hamburg.de/"]
+
+    rules = (
+        Rule(LinkExtractor(allow="studium/studiengaenge-a-z/")),
+        Rule(LinkExtractor(allow="courses/show", deny=("termine/-schulcampus", "termin", "emil", "elearning")), callback="parse_item"),
+    )
+
+    def parse_item(self, response):
+        yield {
+            "content": response.css(".intro span::text").get(),
+            "url": str(response.url)
+        }
