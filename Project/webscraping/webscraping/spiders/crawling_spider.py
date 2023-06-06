f38d3d780214635d64ab81f9ebafd954c0ff1782
kigari94 <stefankulow@web.de>
2023-06-06 17:26:50 +0200

    expand data to be scraped + adding json output

--- Project/webscraping/webscraping/spiders/crawling_spider.py
+++ Project/webscraping/webscraping/spiders/crawling_spider.py
@@ -3,22 +3,26 @@ from scrapy.spiders import CrawlSpider
 
 
 class CrawlingSpider(scrapy.Spider):
-    name = "collegeCrawler"
+    name = "haw"
     allowed_domains = ["haw-hamburg.de"]
     start_urls = ["https://www.haw-hamburg.de/studium/studiengaenge-a-z/"]
 
     def parse(self, response):
-        titles = response.css('div.course_text')
-        for title in titles:
-            name = title.css('a::attr(title)').get()
-            url = "https://www.haw-hamburg.de" + title.css('a::attr(href)').get()
-            print(url)
-            req = scrapy.Request(url, callback=self.parse_subcategory)
+        # Get name and url for all available courses
+        courses = response.css('div.course_text')
+        for course in courses:
+            name = course.css('a::attr(title)').get()
+            url = "https://www.haw-hamburg.de" + course.css('a::attr(href)').get()
+            # Send request to get the respective course pages
+            req = scrapy.Request(url, callback=self.parse_courses)
             req.meta['name'] = name
             yield req
 
-    def parse_subcategory(self, response):
+    def parse_courses(self, response):
+        # Extract required data from each course
         yield {
             'name': response.meta.get('name'),
-            'content': response.css('span.intro::text').get()
+            'intro': response.css('span.intro::text').get(),
+            'content': response.css('div.card-body.black p::text').getall(),
+            'graduation': response.css('div.overview-content.col-12.col-sm-4.col-lg-12 div:nth-child(3)').get()
         }
